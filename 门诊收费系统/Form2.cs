7f4ee17759f4d07c28b2510a4c796c917d4de1cb
李荣耀 <@DESKTOP-NT1HALM>
2021-06-25 00:33:22 +0800

    医生登录界面

--- 门诊收费系统/Form2.cs
+++ 门诊收费系统/Form2.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace 门诊收费系统
 {
@@ -18,6 +19,43 @@ namespace 门诊收费系统
         }
 
         private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        string strcon = "data source=DESKTOP-NT1HALM;initial catalog =门诊挂号收费系统;integrated security = true";
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(strcon))
+            {
+                string strcmd = "select * from Diagnosis where Pname='{0}' and Dname='{1}' and Diatime='{2}'";
+                strcmd = string.Format(strcmd, textBox1.Text.Trim(), textBox8.Text.Trim(), textBox7.Text.Trim());
+                SqlDataAdapter da = new SqlDataAdapter(strcmd, con);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                dataGridView1.DataSource = ds.Tables[0].DefaultView;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
         {
 
         }
--- 门诊收费系统/Program.cs
+++ 门诊收费系统/Program.cs
@@ -18,7 +18,7 @@ namespace 门诊收费系统
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form2());
         }
     }
 }
